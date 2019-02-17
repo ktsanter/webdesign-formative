471675f89138fcc22f98af3597157c9672350be4
SpunkyAardvark123 <ktsanter@gmail.com>
2019-02-16 20:38:34 -0500

    added explore activity

--- js/main.js
+++ js/main.js
@@ -3,8 +3,31 @@
 const app = function () {		
 	const page = {
 		body: null,
-		itemtree: null
+		itemtree: null,
+    explorebutton: null,
+    quizbutton: null,
+    instructions: {},
+    explore: null,
+    restartexplorebutton: null,
+    quiz: null
 	};
+  
+  const ACTIVITY_EXPLORE = "explore";
+  const ACTIVITY_QUIZ = "quiz";
+  
+  const settings = {
+    activity: null,
+    explorestage: null,
+    selectedfolder: null,
+    selectedfile: null
+  };
+  
+  const EXPLORE_SELECTSOURCE = 'source';
+  const EXPLORE_SELECTDEST = 'dest';
+  const EXPLORE_COMPLETE = 'complete';
+  
+  const CLASS_EXPLOREFOLDER = 'selected-explore-folder';
+  const CLASS_EXPLOREFILE = 'selected-explore-file';
   
   const hierarchy = [
       {
@@ -58,10 +81,18 @@ const app = function () {
 	//----------------------------------------
 	function init () {
 		page.body = document.getElementsByTagName('body')[0];
-		page.itemtree = $('#tree1');
+		page.itemtree = $('#hierarchy');
+    page.explorebutton = document.getElementById('btnExplore');
+    page.quizbutton = document.getElementById('btnQuiz');
+    page.instructions[ACTIVITY_EXPLORE] = document.getElementById('instructions0');
+    page.instructions[ACTIVITY_QUIZ] = document.getElementById('instructions1');
+    page.restartexplorebutton = document.getElementById('btnRestartExplore');
+    page.explore = document.getElementById('explore');
+    page.quiz = document.getElementById('quiz');
     	
 		if (_initializeSettings()) {
       _renderPage();
+      _beginActivity(ACTIVITY_EXPLORE);
 		}
 	}
 	
@@ -76,9 +107,207 @@ const app = function () {
 	// page rendering
 	//-----------------------------------------------------------------------------
 	function _renderPage() {
-    $('#hierarchy').tree({data: hierarchy});
-	}	
+    page.itemtree.tree({
+      data: hierarchy,
+      autoOpen: true
+    });
+    var elemTree = document.getElementById('hierarchy');
+    
+    page.itemtree.on('tree.select', function(e) {_treeSelectHandler(e);} );
+    page.explorebutton.addEventListener('click', _handleExploreClick, false);
+    page.quizbutton.addEventListener('click', _handleQuizClick, false);
+    page.restartexplorebutton.addEventListener('click', _handleRestartExploreClick, false);
+  }	
+  
+  function _setControlsBasedOnActivity() {
+    page.explorebutton.disabled = (settings.activity == ACTIVITY_EXPLORE);
+    page.quizbutton.disabled = !(settings.activity == ACTIVITY_EXPLORE);
+    
+    page.instructions[ACTIVITY_EXPLORE].style.display = 'none';
+    page.instructions[ACTIVITY_QUIZ].style.display = 'none';
+    page.instructions[settings.activity].style.display = 'inline-block';
+    
+    if (settings.activity == ACTIVITY_EXPLORE) {
+      page.explore.style.visibility = 'visible';
+      page.quiz.style.visibility = 'hidden';
+      page.restartexplorebutton.style.display = 'inline';
+    } else {
+      page.explore.style.visibility = 'hidden';
+      page.quiz.style.visibility = 'visible';
+      page.restartexplorebutton.style.display = 'none';
+    }
+  }
+  
+  function _beginActivity(newActivity) {
+    settings.activity = newActivity;
+    _setControlsBasedOnActivity();
+    _removeClassFromAllElements(CLASS_EXPLOREFOLDER);
+    _removeClassFromAllElements(CLASS_EXPLOREFILE);
+    page.itemtree.tree('selectNode', null);
+    
+    settings.explorestage = EXPLORE_SELECTSOURCE;
+    settings.selectedfolder = null;
+    settings.selectedfile = null;
+    _exploreDisplay();
+  }
+  
+  function _exploreDisplay() {  
+    var message = '<div id="displayMessage">';
+    
+    if (settings.explorestage == EXPLORE_SELECTSOURCE) {
+      message += 'Click on a folder to begin the path';
+      
+    } else if (settings.explorestage == EXPLORE_SELECTDEST) {
+      message += '<ul>'
+      message += '<li class="explore-display">The relative path begins at the folder <div class="selected-explore-folder-display">' + settings.selectedfolder.name + '</div></li>';
+      message += '</ul>';
+      message += 'Click on a file to end the path';
+      
+    } else if (settings.explorestage == EXPLORE_COMPLETE) {
+      var relativePath = _getRelativePath(settings.selectedfolder, settings.selectedfile);
+      message += '<ul>'
+      message += '<li class="explore-display">The relative path begins at the folder <div class="selected-explore-folder-display">' + settings.selectedfolder.name + '</div></li>';
+      message += '<li class="explore-display">The relative path ends with the file <div class="selected-explore-file-display">' + settings.selectedfile.name + '</div></li>';
+      message += '</ul>';
+      message += 'Hover over this box to see the relative path <span class="pathbox">' + relativePath + '</span>';
+      
+    } else {
+      message += 'Error: unexpected explore stage = (' + settings.explorestage + ')';
+    }
+    
+    message += '</div>';
+    
+    page.explore.innerHTML = message;
+  }
+  
+  function _addClassToNode(node, className) {
+      var div = node.element.getElementsByTagName('div')[0];
+      var span = div.getElementsByTagName('span')[0];
+      span.classList.add(className);  
+  }
+  
+  function _removeClassFromAllElements(className) {
+    var elems = document.getElementsByClassName(className);
+    
+    for (var i = 0; i < elems.length; i++) {
+      if (elems[i].classList.contains(className)) {
+        elems[i].classList.remove(className);
+      }
+    }
+  }
+  
+  function _getRelativePath(folderNode, fileNode) {
+    var done = false;
+    var path = '';
+    var arrPath = [];
+    var arrBacktrack = [];
+    var arrForwardtrack = [];
+    var setPath = new Set();
+    var objPath = {};
+    
+    var curNode = fileNode;
+    var status = '';
+    
+    while (!done) {
+      arrPath.unshift(curNode);
+      setPath.add(curNode.id);
+      objPath[curNode.id] = arrPath.slice(0, arrPath.length);
 
+      if (curNode.getLevel() <= 1) {
+        status = 'atroot';
+        done = true;
+      } else if (curNode.parent == folderNode) {
+        status = 'matchedfolder';
+        done = true;
+      } else {
+        status = 'movingup';
+        curNode = curNode.parent;
+      }
+    }
+    
+    if (status == 'matchedfolder') {
+      arrForwardtrack = arrPath;
+      
+    } else {
+      done = false;
+      var curNode = folderNode.parent;
+
+      while (!done) {
+        arrBacktrack.push(curNode);
+       if (setPath.has(curNode.id)) {
+          done = true;
+        } else {
+          curNode = curNode.parent;
+        }
+      }
+
+      for (var i = 0; i < arrBacktrack.length; i++) {
+        path += '../';
+      }
+
+      arrForwardtrack = objPath[curNode.id].slice(1);
+    }
+
+    for (var i = 0; i < arrForwardtrack.length; i++) {
+      if (i > 0) path += '/';
+      path += arrForwardtrack[i].name;
+    }
+
+    return path;
+  }
+  
+	//------------------------------------------------------------------
+	// handlers
+	//------------------------------------------------------------------
+	function _treeSelectHandler(e) {
+		if (e.node == null) return;
+    if (settings.activity != ACTIVITY_EXPLORE) return;
+    
+    if (_isFolder(e.node) && settings.explorestage == EXPLORE_SELECTSOURCE) {
+      settings.selectedfolder = e.node;
+      _addClassToNode(e.node, CLASS_EXPLOREFOLDER);
+      settings.explorestage = EXPLORE_SELECTDEST
+      
+    } else if (_isFile(e.node)  && settings.explorestage == EXPLORE_SELECTDEST) {
+      settings.selectedfile = e.node;
+      _addClassToNode(e.node, CLASS_EXPLOREFILE);
+      page.itemtree.tree('selectNode', null);
+      settings.explorestage = EXPLORE_COMPLETE;
+      
+    } else if (settings.explorestage == EXPLORE_COMPLETE) {
+      page.itemtree.tree('selectNode', null);
+    }
+    
+    _exploreDisplay();
+	}   
+  
+  function _handleExploreClick() {
+    _beginActivity(ACTIVITY_EXPLORE);
+  }
+  
+  function _handleQuizClick() {
+    _beginActivity(ACTIVITY_QUIZ);
+  }
+  
+  function _handleRestartExploreClick() {
+    _beginActivity(ACTIVITY_EXPLORE);
+  }
+  
+	//---------------------------------------
+	// tree support routines
+	//----------------------------------------	
+	function _isLeaf(node) {
+		return (node.children.length == 0);
+	}
+  
+  function _isFolder(node) {
+    return !_isLeaf(node);
+  }
+  
+  function _isFile(node) {
+    return _isLeaf(node);
+  }
+  
 	return {
 		init: init
  	};
